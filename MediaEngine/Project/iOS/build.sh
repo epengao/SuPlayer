c21f924c92c9f09d0b31a44ca9e5328caf1226f5
epengao <epengao@126.com>
2016-01-22 18:09:02 +0800

    Reset the iOS platform compiling.

--- MediaEngine/Project/iOS/build.sh
+++ MediaEngine/Project/iOS/build.sh
@@ -4,8 +4,29 @@ FMK_NAME="MediaEngine"
 
 WRK_DIR=build
 INSTALL_DIR=${WRK_DIR}/Product/${FMK_NAME}.framework
-DEVICE_DIR=${WRK_DIR}/Debug-iphoneos/${FMK_NAME}.framework
-SIMULATOR_DIR=${WRK_DIR}/Debug-iphonesimulator/${FMK_NAME}.framework
+DEVICE_DIR=${WRK_DIR}/Release-iphoneos/${FMK_NAME}.framework
+SIMULATOR_DIR=${WRK_DIR}/Release-iphonesimulator/${FMK_NAME}.framework
 
-#xcodebuild -configuration "Debug" -target "${FMK_NAME}" -sdk iphoneos clean build
-xcodebuild -configuration "Debug" -target "${FMK_NAME}" -sdk -read_only_relocs suppress iphonesimulator clean build
+xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphoneos clean build
+if [ -d "${DEVICE_DIR}" ]
+then
+rm -rf "${DEVICE_DIR}"
+fi
+mv -f "${INSTALL_DIR}" "${DEVICE_DIR}"
+
+xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphonesimulator clean build
+if [ -d "${SIMULATOR_DIR}" ]
+then
+rm -rf "${SIMULATOR_DIR}"
+fi
+mv -f "${INSTALL_DIR}" "${SIMULATOR_DIR}"
+
+mkdir -p "${INSTALL_DIR}"
+cp -R "${DEVICE_DIR}/" "${INSTALL_DIR}/"
+lipo -create "${DEVICE_DIR}/${FMK_NAME}" "${SIMULATOR_DIR}/${FMK_NAME}" -output "${INSTALL_DIR}/${FMK_NAME}"
+
+rm -rf "${WRK_DIR}/${FMK_NAME}.build"
+rm -rf "${WRK_DIR}/Release-iphoneos"
+rm -rf "${WRK_DIR}/Release-iphonesimulator"
+
+open "${INSTALL_DIR}"
